54a8ebd43f6dad004fe23a60dfee5eb3f988da40
shiiyan <yan39123@gmail.com>
2022-06-26 16:42:27 +0900

    feat: create function hellow world

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,3 @@
-import * as functions from "firebase-functions";
+import helloWorldFunction from "./helloWorld";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = helloWorldFunction;
