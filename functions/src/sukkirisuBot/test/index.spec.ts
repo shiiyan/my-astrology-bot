8aa91c7c6150cdae6cb558a93ea3259c7b83e0ef
shiiyan <yan39123@gmail.com>
2022-07-05 09:04:01 +0900

    test: add should style test sample in index.spec.ts

--- functions/src/sukkirisuBot/test/index.spec.ts
+++ functions/src/sukkirisuBot/test/index.spec.ts
@@ -1,7 +1,8 @@
-import {expect} from "chai";
+import {expect, should} from "chai";
+should();
 
 describe("Typescipt usage suite", () => {
-  it("should be able to execute a test", () => {
+  it("should be able to execute a test in expect style", () => {
     const foo = "bar";
     const beverages = {tea: ["chai", "matcha", "oolong"]};
 
@@ -9,5 +10,15 @@ describe("Typescipt usage suite", () => {
     expect(foo).to.equal("bar");
     expect(foo).to.have.lengthOf(3);
     expect(beverages).to.have.property("tea").with.lengthOf(3);
+  });
+
+  it("should be able to execute a test in should style", () => {
+    const foo = "bar";
+    const beverages = {tea: ["chai", "matcha", "oolong"]};
+
+    foo.should.be.a("string");
+    foo.should.equal("bar");
+    foo.should.have.lengthOf(3);
+    beverages.should.have.property("tea").with.lengthOf(3);
   });
 });
