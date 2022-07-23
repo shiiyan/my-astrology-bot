46b8326f7d9ddbc1fcae2c555a8c675827f13611
shiiyan <yan39123@gmail.com>
2022-07-23 17:18:19 +0900

    test: add test case for getter of squirrel fortune ranking

--- functions/src/sukkirisuBot/domain/squirrelFortuneRanking/squirrelFortuneRanking.spec.ts
+++ functions/src/sukkirisuBot/domain/squirrelFortuneRanking/squirrelFortuneRanking.spec.ts
@@ -4,7 +4,7 @@ import {BirthMonthFortune} from "./birthMonthFortune";
 import {SquirrelFortuneRanking} from "./squirrelFortuneRanking";
 should();
 
-describe("FortuneRanking", () => {
+describe("SquirrelFortuneRanking", () => {
   it("should hold createDate and ranking of birth month fortune", () => {
     const list: BirthMonthFortune[] = [
       {birthMonth: 1, rank: 1, comment: "", luckyColor: ""},
@@ -86,5 +86,79 @@ describe("FortuneRanking", () => {
     reconstructed.should.have.property("birthMonthFortunes");
     reconstructed.should.have.property("birthMonthFortunes").be.a("array");
     reconstructed.should.have.property("birthMonthFortunes").that.have.members(list);
+  });
+
+  it("should get a birth month fortune by rank", () => {
+    const list: BirthMonthFortune[] = [
+      {birthMonth: 1, rank: 1, comment: "", luckyColor: ""},
+      {birthMonth: 2, rank: 2, comment: "", luckyColor: ""},
+      {birthMonth: 3, rank: 3, comment: "", luckyColor: ""},
+      {birthMonth: 4, rank: 4, comment: "", luckyColor: ""},
+      {birthMonth: 5, rank: 5, comment: "", luckyColor: ""},
+      {birthMonth: 6, rank: 6, comment: "", luckyColor: ""},
+      {birthMonth: 7, rank: 7, comment: "", luckyColor: ""},
+      {birthMonth: 8, rank: 8, comment: "", luckyColor: ""},
+      {birthMonth: 9, rank: 9, comment: "", luckyColor: ""},
+      {birthMonth: 10, rank: 10, comment: "", luckyColor: ""},
+      {birthMonth: 11, rank: 11, comment: "", luckyColor: ""},
+      {birthMonth: 12, rank: 12, comment: "", luckyColor: ""},
+    ];
+    const fortuneRanking = SquirrelFortuneRanking.create(list);
+
+    const getResult = fortuneRanking.getFortuneByRank(1);
+
+    getResult.should.have.property("birthMonth").equals(1);
+    getResult.should.have.property("rank").equals(1);
+    getResult.should.have.property("comment").equals("");
+    getResult.should.have.property("luckyColor").equals("");
+  });
+
+  it("should get a birth month fortune by birth month", () => {
+    const list: BirthMonthFortune[] = [
+      {birthMonth: 1, rank: 1, comment: "", luckyColor: ""},
+      {birthMonth: 2, rank: 2, comment: "", luckyColor: ""},
+      {birthMonth: 3, rank: 3, comment: "", luckyColor: ""},
+      {birthMonth: 4, rank: 4, comment: "", luckyColor: ""},
+      {birthMonth: 5, rank: 5, comment: "", luckyColor: ""},
+      {birthMonth: 6, rank: 6, comment: "", luckyColor: ""},
+      {birthMonth: 7, rank: 7, comment: "", luckyColor: ""},
+      {birthMonth: 8, rank: 8, comment: "", luckyColor: ""},
+      {birthMonth: 9, rank: 9, comment: "", luckyColor: ""},
+      {birthMonth: 10, rank: 10, comment: "", luckyColor: ""},
+      {birthMonth: 11, rank: 11, comment: "", luckyColor: ""},
+      {birthMonth: 12, rank: 12, comment: "", luckyColor: ""},
+    ];
+    const fortuneRanking = SquirrelFortuneRanking.create(list);
+
+    const getResult = fortuneRanking.getFortuneByBirthMonth(1);
+
+    getResult.should.have.property("birthMonth").equals(1);
+    getResult.should.have.property("rank").equals(1);
+    getResult.should.have.property("comment").equals("");
+    getResult.should.have.property("luckyColor").equals("");
+  });
+
+  it("should get create date", () => {
+    const createDate: Date = new Date("2022-07-10 10:08:00");
+    const list: BirthMonthFortune[] = [
+      {birthMonth: 1, rank: 1, comment: "", luckyColor: ""},
+      {birthMonth: 2, rank: 2, comment: "", luckyColor: ""},
+      {birthMonth: 3, rank: 3, comment: "", luckyColor: ""},
+      {birthMonth: 4, rank: 4, comment: "", luckyColor: ""},
+      {birthMonth: 5, rank: 5, comment: "", luckyColor: ""},
+      {birthMonth: 6, rank: 6, comment: "", luckyColor: ""},
+      {birthMonth: 7, rank: 7, comment: "", luckyColor: ""},
+      {birthMonth: 8, rank: 8, comment: "", luckyColor: ""},
+      {birthMonth: 9, rank: 9, comment: "", luckyColor: ""},
+      {birthMonth: 10, rank: 10, comment: "", luckyColor: ""},
+      {birthMonth: 11, rank: 11, comment: "", luckyColor: ""},
+      {birthMonth: 12, rank: 12, comment: "", luckyColor: ""},
+    ];
+    const fortuneRanking = SquirrelFortuneRanking.reconstruct(createDate, list);
+
+    const getResult = fortuneRanking.getCreateDate();
+
+    getResult.should.be.instanceOf(Date);
+    getResult.getTime().should.equal(createDate.getTime());
   });
 });
