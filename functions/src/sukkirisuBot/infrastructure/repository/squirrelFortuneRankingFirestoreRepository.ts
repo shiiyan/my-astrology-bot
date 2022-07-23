e48ce84bebb58ed8a3163d535c3ae193e96aa3cf
shiiyan <yan39123@gmail.com>
2022-07-23 11:03:23 +0900

    refactor: add line breaks to squirrel fortune ranking firestore repository

--- functions/src/sukkirisuBot/infrastructure/repository/squirrelFortuneRankingFirestoreRepository.ts
+++ functions/src/sukkirisuBot/infrastructure/repository/squirrelFortuneRankingFirestoreRepository.ts
@@ -31,21 +31,26 @@ export class SquirrelFortuneRankingFirestoreRepository implements SquirrelFortun
    * @memberof SquirrelFortuneRankingFirestoreRepository
    */
   async findByCreateDateWithLock(date: Date): Promise<SquirrelFortuneRanking | undefined> {
-    const {rankingSnapShot, fortunesSnapShot} = await this.database.runTransaction(async (transaction) => {
-      const dateString = moment(date).format("YYYY-MM-DD");
-      const rankingRef = this.database.collection("squirrelFortuneRankings").doc(dateString);
-      const rankingSnapShot = await transaction.get(rankingRef);
-      const fortunesRef = rankingRef.collection("birthMonthFortunes");
-      const fortunesSnapShot = await transaction.get(fortunesRef);
+    const {rankingSnapShot, fortunesSnapShot} = await this.database.runTransaction(
+        async (transaction) => {
+          const dateString = moment(date).format("YYYY-MM-DD");
+          const rankingRef = this.database
+              .collection("squirrelFortuneRankings")
+              .doc(dateString);
+          const rankingSnapShot = await transaction.get(rankingRef);
+          const fortunesRef = rankingRef.collection("birthMonthFortunes");
+          const fortunesSnapShot = await transaction.get(fortunesRef);
 
-      // ensure same query can only perform once per half hour.
-      const halfHour = Number(moment().format("m")) > 30 ? "30" : "00";
-      const currentHour = moment().format("YYYY-MM-DD HH");
-      const logsRef = this.database.collection("squirrelFortuneRankingQueryLogs").doc(currentHour + ":" + halfHour);
-      transaction.create(logsRef, {method: "findByCreateDateWithLock"});
+          // ensure same query can only perform once per half hour.
+          const halfHour = Number(moment().format("m")) > 30 ? "30" : "00";
+          const currentHour = moment().format("YYYY-MM-DD HH");
+          const logsRef = this.database
+              .collection("squirrelFortuneRankingQueryLogs")
+              .doc(currentHour + ":" + halfHour);
+          transaction.create(logsRef, {method: "findByCreateDateWithLock"});
 
-      return {rankingSnapShot, fortunesSnapShot};
-    });
+          return {rankingSnapShot, fortunesSnapShot};
+        });
 
     if (!rankingSnapShot.exists) {
       return;
