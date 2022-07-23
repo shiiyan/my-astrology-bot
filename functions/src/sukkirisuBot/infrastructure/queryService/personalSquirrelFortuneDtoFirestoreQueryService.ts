32627827519906ac2307be247be36d661ba44799
shiiyan <yan39123@gmail.com>
2022-07-23 11:37:17 +0900

    feat: complete implementaiton of personal squirrel fortune query service

--- functions/src/sukkirisuBot/infrastructure/queryService/personalSquirrelFortuneDtoFirestoreQueryService.ts
+++ functions/src/sukkirisuBot/infrastructure/queryService/personalSquirrelFortuneDtoFirestoreQueryService.ts
@@ -1,4 +1,7 @@
 import {firestore} from "firebase-admin";
+import moment from "moment";
+import {BirthMonthFortune} from "../../domain/squirrelFortuneRanking/birthMonthFortune";
+import {SquirrelFortuneRanking} from "../../domain/squirrelFortuneRanking/squirrelFortuneRanking";
 import {PersonalSquirrelFortuneDto} from "../../useCase/personalSquirrelFortune/personalSquirrelFortuneDto";
 import {PersonalSquirrelFortuneDtoQueryServiceInterface as QueryServiceInterface}
   from "../../useCase/personalSquirrelFortune/personalSquirrelFortuneDtoQueryServiceInterface";
@@ -23,12 +26,68 @@ export class PersonalSquirrelFortuneDtoFireStoreQueryService implements QuerySer
   }
 
   /**
+   * Fetch all personal squirrel fortune by date.
    *
-   *
-   * // @return {Promise<PersonalSquirrelFortuneDto[]>}
+   * @param {Date} date
+   * @return {Promise<PersonalSquirrelFortuneDto[]>}
    * @memberof PersonalSquirrelFortuneDtoFireStoreQueryService
    */
-  async fetchAllByDateWithLock(): Promise<PersonalSquirrelFortuneDto[]> {
-    throw new Error("Method not implemented.");
+  async fetchAllByDateWithLock(date: Date): Promise<PersonalSquirrelFortuneDto[]> {
+    const {fortunesSnapShot, birthMonthProfilesSnapShot} = await this.database.runTransaction(
+        async (transaction) => {
+          // get fortunes snap shot.
+          const dateString = moment(date).format("YYYY-MM-DD");
+          const fortunesRef = this.database
+              .collection("squirrelFortuneRankings")
+              .doc(dateString)
+              .collection("birthMonthFortunes");
+          const fortunesSnapShot = await transaction.get(fortunesRef);
+
+          // get birth month profiles snap shot.
+          const birthMonthProfilesRef = this.database
+              .collection("birthMonthProfiles");
+          const birthMonthProfilesSnapShot = await transaction.get(birthMonthProfilesRef);
+
+          // ensure same query can only perform once per half hour.
+          const halfHour = Number(moment().format("m")) > 30 ? "30" : "00";
+          const currentHour = moment().format("YYYY-MM-DD HH");
+          const logsRef = this.database
+              .collection("personalSqiurrelFortuneQueryServiceLogs")
+              .doc(currentHour + ":" + halfHour);
+          transaction.create(logsRef, {method: "fetchAllByDateWithLock"});
+
+          return {fortunesSnapShot, birthMonthProfilesSnapShot};
+        });
+
+    const allMonthFortunes: BirthMonthFortune[] = [];
+    fortunesSnapShot.forEach((fortune) => {
+      const singleMonthFortune = {
+        birthMonth: fortune.data().birthMonth,
+        rank: fortune.data().rank,
+        comment: fortune.data().comment,
+        luckyColor: fortune.data().luckyColor,
+      };
+
+      allMonthFortunes.push(singleMonthFortune);
+    });
+
+    const squirrelFortuneRanking = SquirrelFortuneRanking.reconstruct(date, allMonthFortunes);
+    const personalSquirrelFortunes: PersonalSquirrelFortuneDto[] = [];
+    birthMonthProfilesSnapShot.forEach((birthMonthProfile) => {
+      const name = String(birthMonthProfile.data().name);
+      const birthMonth = Number(birthMonthProfile.data().birthMonth);
+      const singleMonthFortune = squirrelFortuneRanking.getFortuneByBirthMonth(birthMonth);
+      const personalSquirrelFortune: PersonalSquirrelFortuneDto = {
+        name: name,
+        birthMonth: birthMonth,
+        rank: singleMonthFortune.rank,
+        comment: singleMonthFortune.comment,
+      };
+
+      personalSquirrelFortunes.push(personalSquirrelFortune);
+    });
+
+
+    return personalSquirrelFortunes;
   }
 }
