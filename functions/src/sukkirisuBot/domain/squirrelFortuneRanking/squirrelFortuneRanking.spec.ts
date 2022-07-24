7ca2cfce85a0a04c0d814c7ab408693b21f797fc
shiiyan <yan39123@gmail.com>
2022-07-24 17:25:30 +0900

    test: extract common create method in squirrelFortuneRanking.spec.ts

--- functions/src/sukkirisuBot/domain/squirrelFortuneRanking/squirrelFortuneRanking.spec.ts
+++ functions/src/sukkirisuBot/domain/squirrelFortuneRanking/squirrelFortuneRanking.spec.ts
@@ -6,21 +6,7 @@ should();
 
 describe("SquirrelFortuneRanking", () => {
   it("should hold createDate and ranking of birth month fortune", () => {
-    const list: BirthMonthFortune[] = [
-      {birthMonth: 1, rank: 1, comment: "", luckyColor: ""},
-      {birthMonth: 2, rank: 2, comment: "", luckyColor: ""},
-      {birthMonth: 3, rank: 3, comment: "", luckyColor: ""},
-      {birthMonth: 4, rank: 4, comment: "", luckyColor: ""},
-      {birthMonth: 5, rank: 5, comment: "", luckyColor: ""},
-      {birthMonth: 6, rank: 6, comment: "", luckyColor: ""},
-      {birthMonth: 7, rank: 7, comment: "", luckyColor: ""},
-      {birthMonth: 8, rank: 8, comment: "", luckyColor: ""},
-      {birthMonth: 9, rank: 9, comment: "", luckyColor: ""},
-      {birthMonth: 10, rank: 10, comment: "", luckyColor: ""},
-      {birthMonth: 11, rank: 11, comment: "", luckyColor: ""},
-      {birthMonth: 12, rank: 12, comment: "", luckyColor: ""},
-    ];
-
+    const list = createAllMonthBirthMonthFortunes();
     const fortuneRanking = SquirrelFortuneRanking.create(list);
 
     fortuneRanking.should.be.instanceOf(SquirrelFortuneRanking);
@@ -40,20 +26,7 @@ describe("SquirrelFortuneRanking", () => {
   });
 
   it("should throw invalid argument error given month duplicated.", () => {
-    const list: BirthMonthFortune[] = [
-      {birthMonth: 1, rank: 1, comment: "", luckyColor: ""},
-      {birthMonth: 2, rank: 2, comment: "", luckyColor: ""},
-      {birthMonth: 3, rank: 3, comment: "", luckyColor: ""},
-      {birthMonth: 4, rank: 4, comment: "", luckyColor: ""},
-      {birthMonth: 5, rank: 5, comment: "", luckyColor: ""},
-      {birthMonth: 6, rank: 6, comment: "", luckyColor: ""},
-      {birthMonth: 7, rank: 7, comment: "", luckyColor: ""},
-      {birthMonth: 8, rank: 8, comment: "", luckyColor: ""},
-      {birthMonth: 9, rank: 9, comment: "", luckyColor: ""},
-      {birthMonth: 10, rank: 10, comment: "", luckyColor: ""},
-      {birthMonth: 11, rank: 11, comment: "", luckyColor: ""},
-      {birthMonth: 11, rank: 12, comment: "", luckyColor: ""},
-    ];
+    const list: BirthMonthFortune[] = createDuplicatedMonthBirthMonthFortunes();
 
     (() => SquirrelFortuneRanking.create(list)).should.throw(
         InvalidArgumentError, "There is no fortune of month 12."
@@ -62,21 +35,7 @@ describe("SquirrelFortuneRanking", () => {
 
   it("should reconstruct using createDate and list of birth month fortune", () => {
     const createDate: Date = new Date("2022-07-10 10:08:00");
-    const list: BirthMonthFortune[] = [
-      {birthMonth: 1, rank: 1, comment: "", luckyColor: ""},
-      {birthMonth: 2, rank: 2, comment: "", luckyColor: ""},
-      {birthMonth: 3, rank: 3, comment: "", luckyColor: ""},
-      {birthMonth: 4, rank: 4, comment: "", luckyColor: ""},
-      {birthMonth: 5, rank: 5, comment: "", luckyColor: ""},
-      {birthMonth: 6, rank: 6, comment: "", luckyColor: ""},
-      {birthMonth: 7, rank: 7, comment: "", luckyColor: ""},
-      {birthMonth: 8, rank: 8, comment: "", luckyColor: ""},
-      {birthMonth: 9, rank: 9, comment: "", luckyColor: ""},
-      {birthMonth: 10, rank: 10, comment: "", luckyColor: ""},
-      {birthMonth: 11, rank: 11, comment: "", luckyColor: ""},
-      {birthMonth: 12, rank: 12, comment: "", luckyColor: ""},
-    ];
-
+    const list: BirthMonthFortune[] = createAllMonthBirthMonthFortunes();
     const reconstructed = SquirrelFortuneRanking.reconstruct(createDate, list);
 
     reconstructed.should.be.instanceOf(SquirrelFortuneRanking);
@@ -89,20 +48,7 @@ describe("SquirrelFortuneRanking", () => {
   });
 
   it("should get a birth month fortune by rank", () => {
-    const list: BirthMonthFortune[] = [
-      {birthMonth: 1, rank: 1, comment: "", luckyColor: ""},
-      {birthMonth: 2, rank: 2, comment: "", luckyColor: ""},
-      {birthMonth: 3, rank: 3, comment: "", luckyColor: ""},
-      {birthMonth: 4, rank: 4, comment: "", luckyColor: ""},
-      {birthMonth: 5, rank: 5, comment: "", luckyColor: ""},
-      {birthMonth: 6, rank: 6, comment: "", luckyColor: ""},
-      {birthMonth: 7, rank: 7, comment: "", luckyColor: ""},
-      {birthMonth: 8, rank: 8, comment: "", luckyColor: ""},
-      {birthMonth: 9, rank: 9, comment: "", luckyColor: ""},
-      {birthMonth: 10, rank: 10, comment: "", luckyColor: ""},
-      {birthMonth: 11, rank: 11, comment: "", luckyColor: ""},
-      {birthMonth: 12, rank: 12, comment: "", luckyColor: ""},
-    ];
+    const list: BirthMonthFortune[] = createAllMonthBirthMonthFortunes();
     const fortuneRanking = SquirrelFortuneRanking.create(list);
 
     const getResult = fortuneRanking.getFortuneByRank(1);
@@ -114,20 +60,7 @@ describe("SquirrelFortuneRanking", () => {
   });
 
   it("should get a birth month fortune by birth month", () => {
-    const list: BirthMonthFortune[] = [
-      {birthMonth: 1, rank: 1, comment: "", luckyColor: ""},
-      {birthMonth: 2, rank: 2, comment: "", luckyColor: ""},
-      {birthMonth: 3, rank: 3, comment: "", luckyColor: ""},
-      {birthMonth: 4, rank: 4, comment: "", luckyColor: ""},
-      {birthMonth: 5, rank: 5, comment: "", luckyColor: ""},
-      {birthMonth: 6, rank: 6, comment: "", luckyColor: ""},
-      {birthMonth: 7, rank: 7, comment: "", luckyColor: ""},
-      {birthMonth: 8, rank: 8, comment: "", luckyColor: ""},
-      {birthMonth: 9, rank: 9, comment: "", luckyColor: ""},
-      {birthMonth: 10, rank: 10, comment: "", luckyColor: ""},
-      {birthMonth: 11, rank: 11, comment: "", luckyColor: ""},
-      {birthMonth: 12, rank: 12, comment: "", luckyColor: ""},
-    ];
+    const list: BirthMonthFortune[] = createAllMonthBirthMonthFortunes();
     const fortuneRanking = SquirrelFortuneRanking.create(list);
 
     const getResult = fortuneRanking.getFortuneByBirthMonth(1);
@@ -140,20 +73,7 @@ describe("SquirrelFortuneRanking", () => {
 
   it("should get create date", () => {
     const createDate: Date = new Date("2022-07-10 10:08:00");
-    const list: BirthMonthFortune[] = [
-      {birthMonth: 1, rank: 1, comment: "", luckyColor: ""},
-      {birthMonth: 2, rank: 2, comment: "", luckyColor: ""},
-      {birthMonth: 3, rank: 3, comment: "", luckyColor: ""},
-      {birthMonth: 4, rank: 4, comment: "", luckyColor: ""},
-      {birthMonth: 5, rank: 5, comment: "", luckyColor: ""},
-      {birthMonth: 6, rank: 6, comment: "", luckyColor: ""},
-      {birthMonth: 7, rank: 7, comment: "", luckyColor: ""},
-      {birthMonth: 8, rank: 8, comment: "", luckyColor: ""},
-      {birthMonth: 9, rank: 9, comment: "", luckyColor: ""},
-      {birthMonth: 10, rank: 10, comment: "", luckyColor: ""},
-      {birthMonth: 11, rank: 11, comment: "", luckyColor: ""},
-      {birthMonth: 12, rank: 12, comment: "", luckyColor: ""},
-    ];
+    const list: BirthMonthFortune[] = createAllMonthBirthMonthFortunes();
     const fortuneRanking = SquirrelFortuneRanking.reconstruct(createDate, list);
 
     const getResult = fortuneRanking.getCreateDate();
@@ -162,3 +82,37 @@ describe("SquirrelFortuneRanking", () => {
     getResult.getTime().should.equal(createDate.getTime());
   });
 });
+
+const createAllMonthBirthMonthFortunes = (): BirthMonthFortune[] => {
+  return [
+    {birthMonth: 1, rank: 1, comment: "", luckyColor: ""},
+    {birthMonth: 2, rank: 2, comment: "", luckyColor: ""},
+    {birthMonth: 3, rank: 3, comment: "", luckyColor: ""},
+    {birthMonth: 4, rank: 4, comment: "", luckyColor: ""},
+    {birthMonth: 5, rank: 5, comment: "", luckyColor: ""},
+    {birthMonth: 6, rank: 6, comment: "", luckyColor: ""},
+    {birthMonth: 7, rank: 7, comment: "", luckyColor: ""},
+    {birthMonth: 8, rank: 8, comment: "", luckyColor: ""},
+    {birthMonth: 9, rank: 9, comment: "", luckyColor: ""},
+    {birthMonth: 10, rank: 10, comment: "", luckyColor: ""},
+    {birthMonth: 11, rank: 11, comment: "", luckyColor: ""},
+    {birthMonth: 12, rank: 12, comment: "", luckyColor: ""},
+  ];
+};
+
+const createDuplicatedMonthBirthMonthFortunes = (): BirthMonthFortune[] => {
+  return [
+    {birthMonth: 1, rank: 1, comment: "", luckyColor: ""},
+    {birthMonth: 2, rank: 2, comment: "", luckyColor: ""},
+    {birthMonth: 3, rank: 3, comment: "", luckyColor: ""},
+    {birthMonth: 4, rank: 4, comment: "", luckyColor: ""},
+    {birthMonth: 5, rank: 5, comment: "", luckyColor: ""},
+    {birthMonth: 6, rank: 6, comment: "", luckyColor: ""},
+    {birthMonth: 7, rank: 7, comment: "", luckyColor: ""},
+    {birthMonth: 8, rank: 8, comment: "", luckyColor: ""},
+    {birthMonth: 9, rank: 9, comment: "", luckyColor: ""},
+    {birthMonth: 10, rank: 10, comment: "", luckyColor: ""},
+    {birthMonth: 11, rank: 11, comment: "", luckyColor: ""},
+    {birthMonth: 11, rank: 12, comment: "", luckyColor: ""},
+  ];
+};
