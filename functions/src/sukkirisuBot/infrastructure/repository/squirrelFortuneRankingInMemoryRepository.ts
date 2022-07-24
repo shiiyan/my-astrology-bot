a71925373aba6d3fbf346fce4158b3582e586b81
shiiyan <yan39123@gmail.com>
2022-07-25 08:15:21 +0900

    test: create squirrelFortuneRankingInMemoryRepository

---
+++ functions/src/sukkirisuBot/infrastructure/repository/squirrelFortuneRankingInMemoryRepository.ts
@@ -0,0 +1,37 @@
+import {SquirrelFortuneRankingRepositoryInterface} from "../../domain/squirrelFortuneRanking/entityRepositoryInterface";
+import {SquirrelFortuneRanking} from "../../domain/squirrelFortuneRanking/squirrelFortuneRanking";
+
+/**
+ * @export
+ * @class SquirrelFortuneRankingInMemoryRepository
+ * @implements {SquirrelFortuneRankingRepositoryInterface}
+ */
+export class SquirrelFortuneRankingInMemoryRepository implements SquirrelFortuneRankingRepositoryInterface {
+  private data: SquirrelFortuneRanking[] = [];
+
+  /**
+   * Find squirrel fortune ranking from this.data by create date.
+   *
+   * @param {Date} date
+   * @return {(Promise<SquirrelFortuneRanking | undefined>)}
+   * @memberof SquirrelFortuneRankingInMemoryRepository
+   */
+  findByCreateDateWithLock(date: Date): Promise<SquirrelFortuneRanking | undefined> {
+    const found = this.data.find(
+        (squirrelFortuneRanking) => squirrelFortuneRanking.getCreateDate().getTime() === date.getTime()
+    );
+    return new Promise((resolve) => resolve(found));
+  }
+
+  /**
+   * Save squirrel fortune ranking to this.data.
+   *
+   * @param {SquirrelFortuneRanking} squirrelFortuneRanking
+   * @return {Promise<void>}
+   * @memberof SquirrelFortuneRankingInMemoryRepository
+   */
+  save(squirrelFortuneRanking: SquirrelFortuneRanking): Promise<void> {
+    this.data.push(squirrelFortuneRanking);
+    return new Promise((resolve) => resolve());
+  }
+}
