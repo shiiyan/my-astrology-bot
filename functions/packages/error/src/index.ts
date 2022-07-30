d87646f260e1e4ec8a896ac69f9003d564469b98
shiiyan <yan39123@gmail.com>
2022-07-30 22:51:35 +0900

    feat(sukkirisu-function-error): add base class for RuntimeError

--- functions/packages/error/src/index.ts
+++ functions/packages/error/src/index.ts
@@ -1,8 +1,16 @@
 /**
- * Error for an invalid argument.
+ * Base class of RuntimeError
+ *
+ * @class RuntimeError
+ * @extends {Error}
+ */
+class RuntimeError extends Error {}
+
+/**
+ * RuntimeError for an invalid argument.
  *
  * @export
  * @class InvalidArgumentError
  * @extends {Error}
  */
-export class InvalidArgumentError extends Error {}
+export class InvalidArgumentError extends RuntimeError {}
