11164d57fd51b259f4cd1a3229fe90f3f450630f
shiiyan <yan39123@gmail.com>
2022-08-09 21:41:48 +0900

    feat: fetch sukkirisu index html inside helloWorld

--- functions/src/helloWorld.ts
+++ functions/src/helloWorld.ts
@@ -1,8 +1,13 @@
 import * as functions from "firebase-functions";
+import fetch from "node-fetch";
 
-const helloWorldFunction = (_request: functions.https.Request, response: functions.Response) => {
+const helloWorldFunction = async (_request: functions.https.Request, response: functions.Response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+
+  const fetchResponse = await fetch("https://www.ntv.co.jp/sukkiri/sukkirisu/index.html");
+  const body = await fetchResponse.text();
+
+  response.send("Hello from cloud function" + body);
 };
 
 export default helloWorldFunction;
