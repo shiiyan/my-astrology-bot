49813279259548544a034fe4e470022e41b7ce07
shiiyan <yan39123@gmail.com>
2022-08-11 21:03:51 +0900

    refactor: remove fetch from helloworld function

--- functions/src/helloWorld.ts
+++ functions/src/helloWorld.ts
@@ -1,13 +1,11 @@
 import * as functions from "firebase-functions";
-import fetch from "node-fetch";
 
-const helloWorldFunction = async (_request: functions.https.Request, response: functions.Response) => {
+const helloWorldFunction = async (
+    _request: functions.https.Request,
+    response: functions.Response
+) => {
   functions.logger.info("Hello logs!", { structuredData: true });
-
-  const fetchResponse = await fetch("https://www.ntv.co.jp/sukkiri/sukkirisu/index.html");
-  const body = await fetchResponse.text();
-
-  response.send("Hello from cloud function" + body);
+  response.send("Hello from cloud function");
 };
 
 export default helloWorldFunction;
