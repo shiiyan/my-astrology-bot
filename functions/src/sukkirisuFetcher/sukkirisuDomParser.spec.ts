3c294e0b336a288da162fa9400a27f92ad69c19b
shiiyan <yan39123@gmail.com>
2022-08-11 21:05:36 +0900

    feat: create sukkirisuDomParser wip

---
+++ functions/src/sukkirisuFetcher/sukkirisuDomParser.spec.ts
@@ -0,0 +1,300 @@
+
+
+const htmlString = ```
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>誕生月占い スッキりす！｜スッキリ｜日本テレビ</title>
+<meta name="description" content="日本テレビ「スッキリ」の誕生月占い スッキりす！に関するページです。">
+<meta name="author" content="日本テレビ放送網株式会社">
+<meta name="copyright" content="Nippon Television Network Corporation">
+<meta property="og:title" content="誕生月占い スッキりす！">
+<meta property="og:type" content="article">
+<meta property="og:description" content="日本テレビ「スッキリ」の誕生月占い スッキりす！に関するページです。">
+<meta property="og:url" content="https://www.ntv.co.jp/sukkiri/sukkirisu/">
+<meta property="og:image" content="https://www.ntv.co.jp/sukkiri/images/og_image.jpg">
+<meta property="og:site_name" content="日本テレビ">
+<meta property="fb:app_id" content="327091700663212">
+<meta property="og:locale" content="ja_JP">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@ntv_sukkiri">
+<meta name="cXenseParse:recs:recommendable" content="false">
+<link rel="icon" type="image/png" href="/assets/images/meta/icon.png" sizes="196x196">
+<link rel="apple-touch-icon" href="/assets/images/meta/apple-touch-icon.png" sizes="144x144">
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TBHL2WG');</script>
+<!-- End Google Tag Manager -->
+
+<!-- css -->
+<link rel="stylesheet" type="text/css" href="/assets/css/main.css">
+<link rel="stylesheet" type="text/css" href="/assets/css/fonts.css">
+<link rel="stylesheet" type="text/css" href="/assets/css/article.css">
+<link rel="stylesheet" type="text/css" href="/sukkiri/assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="/sukkiri/assets/css/sukkirisu.css">
+
+<!-- js -->
+<script src="../config.js"></script>
+<script src="../static.js"></script>
+<script src="/assets/js/polyfill.min.js"></script>
+<script src="/assets/js/plugins/jquery-3.2.1.min.js"></script>
+  <script src="/sukkiri/assets/js/aside-own-article-ranking.js"></script>
+</head>
+
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBHL2WG"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<div id="ntv">
+  <ntv-container blank id="sukkirisu">
+    <div class="ntv-wrapper">
+      <ntv-program-header></ntv-program-header>
+
+      <div class="ntv-contentsWrap">
+        <div class="ntv-main">
+          <ntv-header-ad></ntv-header-ad>
+
+          <ntv-program-sub-header></ntv-program-sub-header>
+
+          <ntv-nav></ntv-nav>
+
+          <article class="ntv-article">
+            <div class="ntv-box-title">生まれた月で今日の運勢をチェック！</div>
+            <div class="ntv-article-body">
+              <div class="mainVisual">
+                <img src="/sukkiri/assets/images/sukkirisu/mainvisual.jpg" alt="スッキリす誕生月占い">
+              </div>
+
+              <div class="subtitle">
+                <p class="date">8月11日（木）の<span>運勢をチェック！</span></p>
+                <p class="teller">天星術占い　星ひとみ</p>
+              </div>
+
+              <div>
+                <div style="padding-bottom: 1px;">
+                  <div class="ntv-article-contents-main">
+                    <h3 class="rankGroup-2">スッキリす</h3>
+                    <div>
+                      
+    <div>
+      <div class="row1">
+  
+        <div class="rank" style="background-image: url('/sukkiri/assets/images/sukkirisu/rank-02.png');">2位</div>
+    
+        <p class="month"><span>12</span>月</p>
+      </div>
+      <div class="row2">
+        <p>周りの人と円満な関係をつくれそう</p>
+        <div style="background-image: url('/sukkiri/assets/images/sukkirisu/color-silver.png');">シルバー</div>
+      </div>
+    </div>
+  
+                      
+    <div>
+      <div class="row1">
+  
+        <div class="rank" style="background-image: url('/sukkiri/assets/images/sukkirisu/rank-03.png');">3位</div>
+    
+        <p class="month"><span>6</span>月</p>
+      </div>
+      <div class="row2">
+        <p>リーダーやアドバイザーとして活躍できそう</p>
+        <div style="background-image: url('/sukkiri/assets/images/sukkirisu/color-orange.png');">オレンジ</div>
+      </div>
+    </div>
+  
+                      
+    <div>
+      <div class="row1">
+  
+        <div class="rank" style="background-image: url('/sukkiri/assets/images/sukkirisu/rank-04.png');">4位</div>
+    
+        <p class="month"><span>11</span>月</p>
+      </div>
+      <div class="row2">
+        <p>物事を冷静に見ることができる日</p>
+        <div style="background-image: url('/sukkiri/assets/images/sukkirisu/color-black.png');">黒</div>
+      </div>
+    </div>
+  
+                      
+    <div>
+      <div class="row1">
+  
+        <div class="rank" style="background-image: url('/sukkiri/assets/images/sukkirisu/rank-05.png');">5位</div>
+    
+        <p class="month"><span>8</span>月</p>
+      </div>
+      <div class="row2">
+        <p>才能や人柄を評価してくれる人がカギに</p>
+        <div style="background-image: url('/sukkiri/assets/images/sukkirisu/color-green.png');">緑</div>
+      </div>
+    </div>
+  
+                      
+    <div>
+      <div class="row1">
+  
+        <div class="rank" style="background-image: url('/sukkiri/assets/images/sukkirisu/rank-06.png');">6位</div>
+    
+        <p class="month"><span>2</span>月</p>
+      </div>
+      <div class="row2">
+        <p>苦手な事は午前中に終わらせよう</p>
+        <div style="background-image: url('/sukkiri/assets/images/sukkirisu/color-purple.png');">紫</div>
+      </div>
+    </div>
+  
+                    </div>
+
+                    <h3 class="rankGroup-7">まあまあスッキリす</h3>
+                    <div>
+                      
+    <div>
+      <div class="row1">
+  
+        <div class="rank" style="background-image: url('/sukkiri/assets/images/sukkirisu/rank-07.png');">7位</div>
+    
+        <p class="month"><span>4</span>月</p>
+      </div>
+      <div class="row2">
+        <p>メールは直ぐに返信し、スピーディーに</p>
+        <div style="background-image: url('/sukkiri/assets/images/sukkirisu/color-yellow.png');">黄</div>
+      </div>
+    </div>
+  
+                      
+    <div>
+      <div class="row1">
+  
+        <div class="rank" style="background-image: url('/sukkiri/assets/images/sukkirisu/rank-08.png');">8位</div>
+    
+        <p class="month"><span>10</span>月</p>
+      </div>
+      <div class="row2">
+        <p>ピンチのときこそ発想の転換をしよう</p>
+        <div style="background-image: url('/sukkiri/assets/images/sukkirisu/color-pink.png');">ピンク</div>
+      </div>
+    </div>
+  
+                      
+    <div>
+      <div class="row1">
+  
+        <div class="rank" style="background-image: url('/sukkiri/assets/images/sukkirisu/rank-09.png');">9位</div>
+    
+        <p class="month"><span>3</span>月</p>
+      </div>
+      <div class="row2">
+        <p>やり残したことは今日中に整理して</p>
+        <div style="background-image: url('/sukkiri/assets/images/sukkirisu/color-gold.png');">ゴールド</div>
+      </div>
+    </div>
+  
+                      
+    <div>
+      <div class="row1">
+  
+        <div class="rank" style="background-image: url('/sukkiri/assets/images/sukkirisu/rank-10.png');">10位</div>
+    
+        <p class="month"><span>5</span>月</p>
+      </div>
+      <div class="row2">
+        <p>相手の気持ちを考えて発言しよう</p>
+        <div style="background-image: url('/sukkiri/assets/images/sukkirisu/color-red.png');">赤</div>
+      </div>
+    </div>
+  
+                      
+    <div>
+      <div class="row1">
+  
+        <div class="rank" style="background-image: url('/sukkiri/assets/images/sukkirisu/rank-11.png');">11位</div>
+    
+        <p class="month"><span>9</span>月</p>
+      </div>
+      <div class="row2">
+        <p>過去の出来事を引きずらず今を大切に</p>
+        <div style="background-image: url('/sukkiri/assets/images/sukkirisu/color-white.png');">白</div>
+      </div>
+    </div>
+  
+                    </div>
+
+                    <h3 class="rankGroup-12">がっかりす</h3>
+                    <div>
+                      
+    <div>
+      <div class="row1">
+  
+        <p class="month"><span>7</span>月</p>
+      </div>
+      <div class="row2">
+        <p>否定的な考えで雰囲気を壊すかも　笑顔と柔軟性を大切に</p>
+        <div style="background-image: url('/sukkiri/assets/images/sukkirisu/color-brown.png');">茶</div>
+      </div>
+    </div>
+  
+                    </div>
+
+                    <h3 class="rankGroup-1">超スッキリす</h3>
+                    <div>
+                      
+    <div>
+      <div class="row1">
+  
+        <p class="month"><span>1</span>月</p>
+      </div>
+      <div class="row2">
+        <p>新たな挑戦にツキあり　ワンパターンな行動から抜け出すチャンス</p>
+        <div style="background-image: url('/sukkiri/assets/images/sukkirisu/color-blue.png');">青</div>
+      </div>
+    </div>
+  
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="ntv-share">
+                <div class="ntv-share-title">このページを<br>シェアする</div>
+                <div class="ntv-share-body">
+                  <ul>
+                    <li><a class="facebook" href="https://www.facebook.com/sharer/sharer.php?u=https://www.ntv.co.jp/sukkiri/sukkirisu/" target="_blank" el="noopener noreferrer" gtm-share="facebook">シェアする</a></li>
+                    <li><a class="twitter" href="https://twitter.com/share?text=&url=https://www.ntv.co.jp/sukkiri/sukkirisu/&hashtags=%E3%82%B9%E3%83%83%E3%82%AD%E3%83%AA" target="_blank" el="noopener noreferrer" gtm-share="twitter">ツイートする</a></li>
+                    <li><a class="line" href="https://timeline.line.me/social-plugin/share?url=https%3A%2F%2Fwww.ntv.co.jp%2Fsukkiri%2Fsukkirisu%2F" target="_blank" el="noopener noreferrer" gtm-share="line">LINEで送る</a></li>
+                  </ul>
+                </div>
+              </div>
+
+            </div>
+          </article>
+        </div>
+
+        <div class="ntv-aside">
+          <ntv-aside-ad></ntv-aside-ad>
+          <ntv-small-banner></ntv-small-banner>
+          <ntv-aside-movies>番組公式動画</ntv-aside-movies>
+            <div class="ntv-box" id="aside-ranking"></div>
+          <ntv-aside-sns>番組公式SNS</ntv-aside-sns>
+          <ntv-rectangle-banner></ntv-rectangle-banner>
+        </div>
+      </div>
+
+      <ntv-program-footer></ntv-program-footer>
+    </div>
+  </ntv-container>
+</div>
+
+<script src="/assets/js/ntv.js"></script>
+</body>
+</html>
+```;
