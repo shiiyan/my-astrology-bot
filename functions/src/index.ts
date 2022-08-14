de6254c858701238ed1d8e298f72e7be7cb7db44
shiiyan <yan39123@gmail.com>
2022-08-14 12:15:39 +0900

    feat: export fetch sukkirsisu pubsub function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,8 @@
 import * as functions from "firebase-functions";
 import helloWorldFunction from "./helloWorld";
 import boltAppReceiver from "./sukkirisuBot/sukkirisuBot";
+import fetchSukkirisuFunction from "./sukkirisuFetcher/fetchSukkirisuFunction";
 
-export const helloWorld = functions
-    .https
-    .onRequest(helloWorldFunction);
-
-export const slack = functions
-    .region("asia-northeast1")
-    .https
-    .onRequest(boltAppReceiver.app);
+export const helloWorld = functions.https.onRequest(helloWorldFunction);
+export const slack = functions.region("asia-northeast1").https.onRequest(boltAppReceiver.app);
+export const fetchSukkirisu = functions.pubsub.topic("my-pubsub").onPublish(fetchSukkirisuFunction);
