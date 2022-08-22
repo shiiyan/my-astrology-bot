ea11211d3622cf00d9c7857c07b6722fc0a90999
shiiyan <36617009+shiiyan@users.noreply.github.com>
2022-08-22 12:49:13 +0900

    fix: use dummy name in help message

--- functions/src/sukkirisuBot/useCase/showHelpMessage/showHelpMessage.ts
+++ functions/src/sukkirisuBot/useCase/showHelpMessage/showHelpMessage.ts
@@ -38,7 +38,7 @@ export class ShowHelpMessage {
     return `コマンド一覧。コマンドは日本語です
 Sukkirisu Bot スッキりす （全てのランキングを表示）
 Sukkirisu Bot 個人スッキりす （登録されているユーザのランキングを表示）
-Sukkirisu Bot <ユーザー名>は<誕生月>月生まれ （ユーザーを登録。ex. gansekiは12月生まれ）
+Sukkirisu Bot <ユーザー名>は<誕生月>月生まれ （ユーザーを登録。ex. abcは12月生まれ）
 
 0分~30分、31~59分の30分単位で再度リクエストを送ることができるようになります`;
   }
