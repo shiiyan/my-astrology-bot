dffd0b5f6c8a0bfb8c60a5d1856b3ba96efec549
shiiyan <yan39123@gmail.com>
2022-10-23 22:15:41 +0900

    feat(helloWorld): publish message to pubsub

--- functions/src/helloWorld.ts
+++ functions/src/helloWorld.ts
@@ -1,10 +1,26 @@
 import * as functions from "firebase-functions";
+import { PubSub } from "@google-cloud/pubsub";
+
+const topicName = "projects/sukkirisu-d6ac0/topics/my-hello-world";
+const pubsubClient = new PubSub();
+
+const publishMessage = async (): Promise<void> => {
+  try {
+    const messageId = await pubsubClient
+        .topic(topicName)
+        .publishMessage({ data: "Hello, world!" });
+    functions.logger.info(`Message ${messageId} published.`);
+  } catch (e) {
+    functions.logger.error(e);
+  }
+};
 
 const helloWorldFunction = (
     _request: functions.https.Request,
     response: functions.Response
 ) => {
   functions.logger.info("Hello logs!", { structuredData: true });
+  publishMessage();
   response.send("Hello from cloud function");
 };
 
