02d6bd4998aeb85fde503184f60f5cdab4d78e4f
shiiyan <yan39123@gmail.com>
2022-10-30 18:16:41 +0900

    feat: use self introduce function in index

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,11 +3,27 @@ import * as firebaseAdmin from "firebase-admin";
 import helloWorldFunction from "./helloWorld";
 import boltAppReceiver from "./sukkirisuBot/sukkirisuBotFunction";
 import fetchSukkirisuFunction from "./sukkirisuFetcher/fetchSukkirisuFunction";
+import selfIntroduceFunction from "./sukkirisuSubscriber/selfIntroduceFunction";
 import dotenv from "dotenv";
 
 dotenv.config();
 firebaseAdmin.initializeApp();
 
-export const helloWorld = functions.https.onRequest(helloWorldFunction);
-export const slack = functions.region("asia-northeast1").https.onRequest(boltAppReceiver.app);
-export const fetchSukkirisu = functions.pubsub.topic("my-pubsub").onPublish(fetchSukkirisuFunction);
+export const helloWorld = functions
+    .https
+    .onRequest(helloWorldFunction);
+
+export const slack = functions
+    .region("asia-northeast1")
+    .https
+    .onRequest(boltAppReceiver.app);
+
+export const fetchSukkirisu = functions
+    .pubsub
+    .topic("my-pubsub")
+    .onPublish(fetchSukkirisuFunction);
+
+export const sukkirisuBotSelfIntroduce = functions
+    .pubsub
+    .topic("sukkirisu-bot-self-introduce")
+    .onPublish(selfIntroduceFunction);
