efd6b98f64c35e2c12140ebccafea7006612ae7c
shiiyan <yan39123@gmail.com>
2022-11-03 21:57:16 +0900

    Revert "chore(nodejsCI): test if always in srcs"
    This reverts commit 99fa66a86986e628901aaec3e1c540479f5aeb57.

--- functions/src/index.spec.ts
+++ functions/src/index.spec.ts
@@ -19,7 +19,7 @@ describe("helloWorldFunction", () => {
     const request = {} as functions.https.Request;
     const response = {
       send: (body: string) => {
-        body.should.equal("Hello from cloud functi");
+        body.should.equal("Hello from cloud function");
       },
     } as functions.Response;
 
