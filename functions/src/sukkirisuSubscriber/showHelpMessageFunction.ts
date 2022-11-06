fc633000feb8dfdce7a64bd750a002cba0e9db08
shiiyan <yan39123@gmail.com>
2022-11-06 23:06:24 +0900

    fix(sukkirisuSubscriber): add space to show help message function

--- functions/src/sukkirisuSubscriber/showHelpMessageFunction.ts
+++ functions/src/sukkirisuSubscriber/showHelpMessageFunction.ts
@@ -30,7 +30,7 @@ const showHelpMessageFunction = async (message: {data: string}): Promise<void> =
                 .concat("\n@Sukkirisu Bot <ユーザ名>は<誕生月>月生まれ （ユーザを登録。ユーザ名は英語。例、abcは12月生まれ）")
                 .concat("\n@Sukkirisu Bot ヘルプ （ヘルプメッセージを表示）")
                 .concat("\n@Sukkirisu Bot 自己紹介 （スッキりすボットの自己紹介を見る）```")
-                .concat("コマンドから返信まで時間がかかる場合があります。"),
+                .concat(" コマンドから返信まで時間がかかる場合があります。"),
           },
         },
       ],
