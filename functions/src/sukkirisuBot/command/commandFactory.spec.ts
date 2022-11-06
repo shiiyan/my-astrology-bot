f52280b8c59dd42026481838575557edccc60ebf
shiiyan <yan39123@gmail.com>
2022-11-06 19:21:16 +0900

    feat(sukkrisuBot): make show help comd in factory

--- functions/src/sukkirisuBot/command/commandFactory.spec.ts
+++ functions/src/sukkirisuBot/command/commandFactory.spec.ts
@@ -1,6 +1,7 @@
 import { should } from "chai";
 import { CommandFactory } from "./commandFactory";
 import { SelfIntroduceCommand } from "./selfIntroduceCommand";
+import { ShowHelpMessageCommand } from "./showHelpMessageCommand";
 should();
 
 describe("CommandFactory", () => {
@@ -12,8 +13,34 @@ describe("CommandFactory", () => {
       ts: "current",
       channel: "channel_mocha",
       event_ts: "current",
+    }) ?? false;
+
+    command.should.be.an.instanceOf(SelfIntroduceCommand);
+  });
+
+  it("should make show help message command given correct message", () => {
+    const command = CommandFactory.make({
+      type: "app_mention",
+      username: "test_user",
+      text: "ヘルプ",
+      ts: "current",
+      channel: "channel_mocha",
+      event_ts: "current",
+    }) ?? false;
+
+    command.should.be.an.instanceOf(ShowHelpMessageCommand);
+  });
+
+  it("should return undefined when no command to make", () => {
+    const command = CommandFactory.make({
+      type: "app_mention",
+      username: "test_user",
+      text: "ご飯を食べて",
+      ts: "current",
+      channel: "channel_mocha",
+      event_ts: "current",
     });
 
-    command?.should.be.an.instanceOf(SelfIntroduceCommand);
+    should().not.exist(command);
   });
 });
--- functions/src/sukkirisuBot/command/commandFactory.ts
+++ functions/src/sukkirisuBot/command/commandFactory.ts
@@ -1,6 +1,7 @@
 import { AppMentionEvent } from "@slack/bolt";
 import { CommandInterface } from "./commandInterface";
 import { SelfIntroduceCommand } from "./selfIntroduceCommand";
+import { ShowHelpMessageCommand } from "./showHelpMessageCommand";
 
 /**
  * @export
@@ -10,10 +11,10 @@ export class CommandFactory {
   /**
    * @static
    * @param {AppMentionEvent} event
-   * @return {*}  {(CommandInterface | null)}
+   * @return {*}  {(CommandInterface | undefined)}
    * @memberof CommandFactory
    */
-  public static make(event: AppMentionEvent): CommandInterface | null {
+  public static make(event: AppMentionEvent): CommandInterface | undefined {
     let matchedGroups: {[key: string]: string} | undefined;
 
     matchedGroups = event.text.match(/(?<selfIntroduce>自己紹介)/)?.groups;
@@ -23,9 +24,9 @@ export class CommandFactory {
 
     matchedGroups = event.text.match(/(?<help>help|ヘルプ)/)?.groups;
     if (matchedGroups?.help) {
-      // return new ShowHelpMessageCommand();
+      return new ShowHelpMessageCommand(event);
     }
 
-    return null;
+    return;
   }
 }
