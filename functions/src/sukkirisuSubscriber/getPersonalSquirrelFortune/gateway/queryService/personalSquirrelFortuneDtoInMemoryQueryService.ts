071e18f20787c08a5896895dfed34c64bd3afa37
shiiyan <yan39123@gmail.com>
2022-11-13 22:55:51 +0900

    feat(sukkirisuSub): relocate personalSquirrelFortune query service

---
+++ functions/src/sukkirisuSubscriber/getPersonalSquirrelFortune/gateway/queryService/personalSquirrelFortuneDtoInMemoryQueryService.ts
@@ -0,0 +1,47 @@
+import { PersonalSquirrelFortuneDtoQueryServiceInterface as QueryServiceInterface }
+  from "../../../../sukkirisuBot/useCase/personalSquirrelFortune/personalSquirrelFortuneDtoQueryServiceInterface";
+import { PersonalSquirrelFortuneDto } from "../../useCase/personalSquirrelFortuneDto";
+
+type FortunesWithDate = {
+    createDate: Date,
+    personalSquirrelFortunes: PersonalSquirrelFortuneDto[]
+};
+
+/**
+ * @export
+ * @class PersonalSquirrelFortuneDtoInMemoryQueryService
+ * @implements {QueryServiceInterface}
+ */
+export class PersonalSquirrelFortuneDtoInMemoryQueryService implements QueryServiceInterface {
+  private data: FortunesWithDate[] = [];
+
+  /**
+   * Fetch all personal squirrel fortunes from this.data by date.
+   *
+   * @param {Date} date
+   * @return {*} {Promise<PersonalSquirrelFortuneDto[] | undefined>}
+   * @memberof PersonalSquirrelFortuneDtoInMemoryQueryService
+   */
+  fetchAllByDate(date: Date): Promise<PersonalSquirrelFortuneDto[] | undefined> {
+    const found = this.data.find(
+        (fortuneWithDate) => fortuneWithDate.createDate.getTime() === date.getTime()
+    );
+
+    return new Promise((resolve) => resolve(found?.personalSquirrelFortunes));
+  }
+
+  /**
+   * Save personal squirrel fortunes to this.data with date for testing.
+   *
+   * @param {Date} date
+   * @param {PersonalSquirrelFortuneDto[]} personalSquirrelFortunes
+   * @memberof PersonalSquirrelFortuneDtoInMemoryQueryService
+   */
+  saveAll(date: Date, personalSquirrelFortunes: PersonalSquirrelFortuneDto[]): void {
+    const fortuneWithDate = {
+      createDate: date,
+      personalSquirrelFortunes: personalSquirrelFortunes,
+    };
+    this.data.push(fortuneWithDate);
+  }
+}
